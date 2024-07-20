7fac45d11fc2210d6bae2adbe6189b435105eed0
Kapil Khedkar <31789869+kapilkhedkar@users.noreply.github.com>
2024-07-20 18:31:12 +0530

    Project created

---
+++ PawPics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PawPics
+//
+//  Created by Kapil Khedkar on 20/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'PawPics' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PawPics
+  #pod 'MyDogsGallery', '~> 0.2.0'
+
+end
